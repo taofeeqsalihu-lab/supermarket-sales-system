f6c0a8b4e2114ba904b4998d589aa3a31589db6c
taofeeqsalihu-lab <tafiquesalihu@gmail.com>
2026-07-20 12:36:47 +0100

    Update urls.py

--- backend/supermarket/urls.py
+++ backend/supermarket/urls.py
@@ -1,1 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from inventory.views import ProductViewSet
+from sales.views import SaleViewSet
+
+router = routers.DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'sales', SaleViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
 
