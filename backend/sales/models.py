259e4f8bb4e83fe5ed601beedb3010084286b5ee
taofeeqsalihu-lab <tafiquesalihu@gmail.com>
2026-07-20 12:33:14 +0100

    Update models.py

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+from inventory.models import Product
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity_sold = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity_sold}"
 
