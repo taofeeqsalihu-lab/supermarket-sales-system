c52d3af29ab3775d44d61fe1d02de495c90d0ad3
taofeeqsalihu-lab <tafiquesalihu@gmail.com>
2026-07-20 12:32:10 +0100

    Update settings.py

--- backend/supermarket/settings.py
+++ backend/supermarket/settings.py
@@ -1,1 +1,22 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supermarket_db',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'inventory',
+    'sales',
+]
 
