4ec110b5fa96c2aee95df251596222e834a6dded
taofeeqsalihu-lab <tafiquesalihu@gmail.com>
2026-07-20 12:32:43 +0100

    Update models.py

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_in_stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
